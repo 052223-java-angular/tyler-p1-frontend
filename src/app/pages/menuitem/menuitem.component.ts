90ca557cf86a25df1f4c06df0f707ed13ecb5df2
Tyler Johnston <tylergarrettjohnston@gmail.com>
2023-06-16 19:41:59 -0400

    Get adding and removing menu items to and from cart working

--- src/app/pages/menuitem/menuitem.component.ts
+++ src/app/pages/menuitem/menuitem.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { MessageService } from 'primeng/api';
+import { finalize } from 'rxjs';
+import { AppSettings } from 'src/app/global/app-settings';
+import { CartMenuItemOffer } from 'src/app/models/cart-menu-item-offer';
 import { MenuItem } from 'src/app/models/menu-item';
+import { CartService } from 'src/app/services/cart.service';
+import { TokenService } from 'src/app/services/token.service';
 
 @Component({
   selector: 'app-menuitem',
@@ -9,8 +15,16 @@ import { MenuItem } from 'src/app/models/menu-item';
 })
 export class MenuItemComponent implements OnInit {
   menuItem!: MenuItem;
+  cartMenuItemOffer!: CartMenuItemOffer;
+  componentLoaded = false;
+  user: any;
 
-  constructor(private router: Router) {
+  constructor(
+    private messageService: MessageService,
+    private cartService: CartService,
+    private tokenService: TokenService,
+    private router: Router
+  ) {
     this.menuItem =
       this.router.getCurrentNavigation()?.extras.state?.['menuItem'];
   }
@@ -21,5 +35,84 @@ export class MenuItemComponent implements OnInit {
     for (let menuSection of this.menuItem.menuSections) {
       menuSection.menuItems.sort((a, b) => a.displayOrder - b.displayOrder);
     }
+    this.user = this.tokenService.getUser();
+
+    if (this.user.id) {
+      this.cartService
+        .getCartMenuItemOffer(this.menuItem.menuItemOffers[0].id)
+        .pipe(
+          finalize(() => {
+            this.componentLoaded = true;
+          })
+        )
+        .subscribe({
+          next: (data) => {
+            if (data) {
+              this.cartMenuItemOffer = data;
+            }
+          },
+          error: (error) => {
+            this.messageService.add({
+              severity: 'error',
+              summary: 'Error',
+              detail: error.error.message,
+              life: AppSettings.DEFAULT_MESSAGE_LIFE,
+            });
+          },
+        });
+    } else {
+      this.componentLoaded = true;
+    }
+  }
+
+  addCartMenuItemOffer() {
+    this.cartService
+      .addCartMenuItemOffer({
+        menuItemOfferId: this.menuItem.menuItemOffers[0].id,
+        quantity: 1,
+      })
+      .subscribe({
+        next: () => {
+          this.messageService.add({
+            severity: 'success',
+            summary: 'Success',
+            detail: `${this.menuItem.name} successfully added to cart`,
+            life: AppSettings.DEFAULT_MESSAGE_LIFE,
+          });
+          this.router.navigate(['/']);
+        },
+        error: (error) => {
+          this.messageService.add({
+            severity: 'error',
+            summary: 'Error',
+            detail: error.error.message,
+            life: AppSettings.DEFAULT_MESSAGE_LIFE,
+          });
+        },
+      });
+  }
+
+  removeCartMenuItemOffer() {
+    this.cartService
+      .removeCartMenuItemOffer(this.cartMenuItemOffer.id)
+      .subscribe({
+        next: () => {
+          this.messageService.add({
+            severity: 'success',
+            summary: 'Success',
+            detail: `${this.menuItem.name} successfully removed from cart`,
+            life: AppSettings.DEFAULT_MESSAGE_LIFE,
+          });
+          this.router.navigate(['/']);
+        },
+        error: (error) => {
+          this.messageService.add({
+            severity: 'error',
+            summary: 'Error',
+            detail: error.error.message,
+            life: AppSettings.DEFAULT_MESSAGE_LIFE,
+          });
+        },
+      });
   }
 }
---
+++ src/app/services/cart.service.ts
@@ -0,0 +1,31 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { AppSettings } from '../global/app-settings';
+import { Observable } from 'rxjs';
+import { CartMenuItemOffer } from '../models/cart-menu-item-offer';
+import { AddCartMenuItemOfferPayload } from '../models/add-cart-menu-item-offer-payload';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CartService {
+  baseUrl = `${AppSettings.P1_API_URL}/cart`;
+
+  constructor(private http: HttpClient) {}
+
+  getCartMenuItemOffer(menuItemOfferId: string): Observable<CartMenuItemOffer> {
+    return this.http.get<CartMenuItemOffer>(
+      `${this.baseUrl}/menuitemoffers/${menuItemOfferId}`
+    );
+  }
+
+  addCartMenuItemOffer(payload: AddCartMenuItemOfferPayload): Observable<void> {
+    return this.http.post<void>(`${this.baseUrl}/menuitemoffers`, payload);
+  }
+
+  removeCartMenuItemOffer(cartMenuItemOfferId: string): Observable<void> {
+    return this.http.delete<void>(
+      `${this.baseUrl}/menuitemoffers/${cartMenuItemOfferId}`
+    );
+  }
+}
