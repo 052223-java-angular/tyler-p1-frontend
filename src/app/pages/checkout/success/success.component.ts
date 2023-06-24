d734b2b0c9a4a643f57a34e2e42527e305299aa5
Tyler Johnston <tylergarrettjohnston@gmail.com>
2023-06-24 16:47:05 -0400

    Touch up checkout success page

--- src/app/pages/checkout/success/success.component.ts
+++ src/app/pages/checkout/success/success.component.ts
@@ -1,13 +1,39 @@
 import { Component, OnInit } from '@angular/core';
 import { NgEventBus } from 'ng-event-bus';
+import { MessageService } from 'primeng/api';
+import { AppSettings } from 'src/app/global/app-settings';
 import { EventBusEvents } from 'src/app/global/event-bus-events';
+import { OrderService } from 'src/app/order.service';
 @Component({
   selector: 'app-success',
   templateUrl: './success.component.html',
   styleUrls: ['./success.component.css'],
 })
 export class SuccessComponent implements OnInit {
-  constructor(private eventBus: NgEventBus) {}
+  order: any;
+  constructor(
+    private messageService: MessageService,
+    private orderService: OrderService,
+    private eventBus: NgEventBus
+  ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.getLatest();
+  }
+
+  getLatest() {
+    this.orderService.getLatest().subscribe({
+      next: (order) => {
+        this.order = order;
+      },
+      error: (error) => {
+        this.messageService.add({
+          severity: 'error',
+          summary: 'Error',
+          detail: error.error.message,
+          life: AppSettings.DEFAULT_MESSAGE_LIFE,
+        });
+      },
+    });
+  }
 }
